fa9f1c47db043d97d6c04cfb6a0b80542fa7ffb4
Victoria Henderson <victoriahenderson27@gmail.com>
2018-05-10 21:25:14 -0400

    Javascript
    decided to try out javascript instead of a jquery plug in

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+var input = document.getElementById('search');
+var filter;
+var photo = document.querySelectorAll('a[data-title]');
+var text;
+
+    input.addEventListener('keyup', function(event){
+      filter = input.value.toLowerCase();
+      for (var i = 0; i < 12; i++ ){
+        text = photo[i].getAttribute("data-title");
+        if (text.toLowerCase().includes(filter)){
+          photo[i].style.display='flex';
+        } else {
+          photo[i].style.display = 'none';
+        }
+      }
+    });
